7a20002526161bd1b3eed0990c4ed8a456de440e
Jerry Chen <jzc23@pitt.edu>
2026-05-21 15:03:19 -0400

    Add: mergeSort - submission-2

---
+++ Data Structures & Algorithms/mergeSort/submission-2.py
@@ -0,0 +1,44 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def mergeSort(self, pairs: List[Pair]) -> List[Pair]:
+        if not pairs:
+            return []
+
+        def merge(arr, L, M, R):
+            left_arr, right_arr = arr[L:M+1], arr[M+1:R+1] # create the left and right arrays
+            left_ptr, right_ptr, ptr = 0, 0, L # indicies of l and r arr and then a global index for the actual array
+
+            while left_ptr < len(left_arr) and right_ptr < len(right_arr):
+                if left_arr[left_ptr].key <= right_arr[right_ptr].key:
+                    arr[ptr] = left_arr[left_ptr]
+                    left_ptr += 1
+                else:
+                    arr[ptr] = right_arr[right_ptr]
+                    right_ptr += 1
+                ptr += 1
+
+            while left_ptr < len(left_arr):
+                arr[ptr] = left_arr[left_ptr]
+                ptr += 1
+                left_ptr += 1  
+
+            while right_ptr < len(right_arr):
+                arr[ptr] = right_arr[right_ptr]
+                ptr += 1
+                right_ptr += 1    
+
+            return arr                
+
+        def sort(arr, L, R):
+            if L == R: # base case where we have 1 element
+                return arr
+            M = (L + R) // 2
+            sort(arr, L, M) # sort the left array
+            sort(arr, M+1, R) # sort the right arry   
+            merge(arr, L, M, R) # merge the two arrays
+            return arr
+        return sort(pairs, 0, len(pairs) - 1)    
