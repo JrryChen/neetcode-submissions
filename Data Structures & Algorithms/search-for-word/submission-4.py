30147481a0c01ea74195b09a00addb176d1488f5
Jerry Chen <jzc23@pitt.edu>
2026-06-10 13:17:45 -0400

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        directions = [
+            (1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1)
+        ]
+        def dfs(curr, r, c, seen):
+            if curr == word:
+                return True
+            for dx, dy in directions:
+                new_r, new_c = r + dx, c + dy
+                if not (0 <= new_r < len(board)) or not (0 <= new_c < len(board[0])):
+                    continue
+                if (new_r, new_c) in seen:
+                    continue
+                ch = board[new_r][new_c]
+                if word.startswith(curr + ch):
+                    seen.add((new_r, new_c))
+                    if dfs(curr + ch, new_r, new_c, seen):
+                        return True
+                    seen.remove((new_r, new_c))
+            return False                    
+
+        for r in range(len(board)):
+            for c in range(len(board[r])):
+                ch = board[r][c]
+                if ch is word:
+                    return True
+                if word.startswith(ch):
+                    seen = {(r, c)}
+                    if dfs(ch, r, c, seen):
+                        return True
+        return False                   
